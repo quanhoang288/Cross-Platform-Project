c041ce1fa960986dbe94b74719a625736992fa9a
trung <trungvuhoangubqn@gmail.com>
2021-11-18 22:38:59 +0700

    chat-api update

--- src/screens/message/ChatScreen.js
+++ src/screens/message/ChatScreen.js
@@ -1,11 +1,80 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect, useCallback, useRef} from 'react';
 import {View, ScrollView, Text, Button, StyleSheet} from 'react-native';
 import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
+import {io} from 'socket.io-client';
+import axios from 'axios';
 const ChatScreen = () => {
+  const socket = useRef();
+  const [currentChat, setCurrent, ] = useState(null);
+  const [Chatmessages, setChatmessages] = useState(null);
+  const [chatList, setChatList] = useState([]);
   const [messages, setMessages] = useState([]);
+  const [arrivalMessage, setArrivalMessage] = useState(null);
+  
+  const chatId ="619526c2cc375b3fb8d08717";
+  const fetchMessage = async () => {
+    try {
+        const res = await axios.get(`http://localhost:8000/api/v1/chats/getMessages/${chatId}/`,);
+        setChatList(res.data);
+    } catch (err) {
+        console.log(err);
+    }
+  }
+
+
+  const sendMessage = async (content) => {
+    const newMessage = {
+      "receivedId":"618e992874550a22a4cb2a98",
+      "chatId": "619526c2cc375b3fb8d08717",
+      "member": [
+          {"_id":"618e992874550a22a4cb2a98"},
+          {"_id":"618e975874550a22a4cb2a90"}
+      ],
+      "content": content,
+       "type": "PRIVATE_CHAT"
+    };
+
+    try {
+        const res = await axios.post(`http://localhost:8000/api/v1/chats/send/`,newMessage);
+        return res.data;
+    } catch (err) {
+        console.log(err);
+    }
+  }
+
+  const handleSubmit = async () => {
+
+    let receiverId = "618e992874550a22a4cb2a98";
+  
+    if (messages !== '') {
+
+        // if (currentChat) {
+            await sendMessage(messages);
+            socket.current.emit('sendMessage', {
+              senderId: "618e975874550a22a4cb2a90",
+              receivedId: receiverId,
+              member: [
+                  {_id:"618e992874550a22a4cb2a98"},
+                  {_id:"618e975874550a22a4cb2a90"}
+              ],
+              content: newMassage,
+              type: "PRIVATE_CHAT",
+            });
+            // addNewMessage();
+        // }
+        // else {
+        //     const newConversation = await initializeConversation(user._id, newUserId)
+        //     await saveMessage(receiverId, content);
+
+        //     setCurrentChat(newConversation);
+        //     setNewUserId(null);
+        // }
+    }
+
+  }
+  // const [messages, setMessages] = useState([]);
   <Button
         title={'React Native Elements'}
         containerStyle={{
@@ -14,7 +83,21 @@ const ChatScreen = () => {
             marginVertical: 10,
         }}
   />
+  useEffect(() =>{
+    socket.current = io('ws://localhost:3000');
+    fetchMessage();
+    socket.current.on('getMessage', async (data) => {
+      //if receivedID !== current.User._Id
+      setArrivalMessage({
+          sender: data.senderId,
+          content: data.content, 
+          createdAt: Date.now(),
+      });
+    
+  });
+  })
   useEffect(() => {
+    
     setMessages([
       {
         _id: 1,
@@ -26,6 +109,7 @@ const ChatScreen = () => {
           avatar: require('../../../assets/avatar.jpg'),
         },
       },
+      
       {
         _id: 2,
         text: require('../../../assets/avatar.jpg'),
@@ -42,6 +126,7 @@ const ChatScreen = () => {
   const onSend = useCallback((messages = []) => {
     setMessages((previousMessages) =>
       GiftedChat.append(previousMessages, messages),
+      
     );
   }, []);
 
@@ -87,10 +172,13 @@ const ChatScreen = () => {
   return (
     <GiftedChat
       messages={messages}
+      handleSubmit={handleSubmit}
       onSend={(messages) => onSend(messages)}
+      
       user={{
         _id: 1,
       }}
+      
       renderBubble={renderBubble}
       alwaysShowSend
       renderSend={renderSend}
