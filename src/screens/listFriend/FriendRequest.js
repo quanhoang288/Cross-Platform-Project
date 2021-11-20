19a6c8a9d86dd24ed0568609f876e1466ab24660
NXTung1102000 <nxtung1102000@gmail.com>
2021-11-20 21:23:16 +0700

    add & test API get-list-friend vs get-list-friend -request

--- src/screens/listFriend/FriendRequest.js
+++ src/screens/listFriend/FriendRequest.js
@@ -1,66 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import ProfileItem from '../../components/account/ProfileItem.jsx'
+import { friend } from '../../apis';
 
 const FriendRequest = () =>{
+
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im54dHVuZyIsImlkIjoiNjE4ZDJiNzQzNjhhZDgzYTk4YzgyMGMwIiwiaWF0IjoxNjM2NzI4NTc2fQ.ZowtOOrPquHHRWKLL_l8fAWdnP1q0Qde8JkiiOsNpu0";
+
+    // API getListFriendRequest
+    const [listFriendRequests, setListFriendRequest] = useState([]);
+    useEffect(() => { 
+        friend.getListFriendRequests(token)
+        .then(result => {
+            // console.log(result.data);
+            const curRequest = result.data.data.friends;
+            setListFriendRequest(curRequest);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, [])
+
+
   return(
-    <View style={styles.container}>
-      <ProfileItem
-        avatar={{
-          source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWx8bMzJMOdeGAEPuEkV3SVhHS6LwUaxVpCy7f3D95lDl8WVRlOewHb3_2QwnNUbPJFDY&usqp=CAU",
-        }}
-        title='QuanHH'
-        displayButtonGroup={true}
-        button={{
-          buttonAccept: 'accept',
-          buttonDelete: 'delete'
-        }}
-        displayDescription={true}
-        time = "1d"
-      />
-      <ProfileItem
-        avatar={{
-          source:"https://phunugioi.com/wp-content/uploads/2020/10/hinh-anh-chibi-buon.jpg",
-        }}
-        title='NXTung'
-        displayButtonGroup={true}
-        button={{
-          buttonAccept: 'accept',
-          buttonDelete: 'delete'
-        }}
-        displayDescription={true}
-        time = "1w"
-      />
-
-      <ProfileItem
-        avatar={{
-          source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7vFTlbgSjNtZgYf3JdY6F7qW4wE_FR2ONaw&usqp=CAU",
-        }}
-        title='TrungVH'
-        displayButtonGroup={true}
-        button={{
-          buttonAccept: 'accept',
-          buttonDelete: 'delete'
-        }}
-        displayDescription={true}
-        time = "3w"
-      />
-
-      <ProfileItem
-        avatar={{
-          source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf5pUFWedzehaMYG74PAWQ3shRdtygnHDfNg&usqp=CAU",
-        }}
-        title='A Girl'
-        displayButtonGroup={true}
-        button={{
-          buttonAccept: 'accept',
-          buttonDelete: 'delete'
-        }}
-        displayDescription={true}
-        time = "2m"
-      />
-    </View>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          {listFriendRequests.map(request => (
+            <ProfileItem
+              key={request._id}
+              avatar={{
+                source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWx8bMzJMOdeGAEPuEkV3SVhHS6LwUaxVpCy7f3D95lDl8WVRlOewHb3_2QwnNUbPJFDY&usqp=CAU",
+              }}
+              title={request.username}
+              displayButtonGroup={true}
+              button={{
+                buttonAccept: 'accept',
+                buttonDelete: 'delete'
+              }}
+              displayDescription={true}
+              time={request.createdAt}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </>
   )
 }
 
--- src/screens/listFriend/ListFriend.js
+++ src/screens/listFriend/ListFriend.js
@@ -1,42 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import ProfileItem from '../../components/account/ProfileItem.jsx'
+import { friend } from '../../apis';
+
 
 const ListFriend = () =>{
+
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im54dHVuZyIsImlkIjoiNjE4ZDJiNzQzNjhhZDgzYTk4YzgyMGMwIiwiaWF0IjoxNjM2NzI4NTc2fQ.ZowtOOrPquHHRWKLL_l8fAWdnP1q0Qde8JkiiOsNpu0";
+
+    // API getListFriend
+    const [listFriend, setListFriend] = useState([]);
+    useEffect(() => {
+        friend.getListFriends(null, token)
+        .then(result => {
+            const curListFr = result.data.data.friends;
+            // console.log(curListFr);
+            setListFriend(curListFr);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }, [])
+
   return(
-    <View style={styles.container}>
-      <ProfileItem
-        avatar={{
-          source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWx8bMzJMOdeGAEPuEkV3SVhHS6LwUaxVpCy7f3D95lDl8WVRlOewHb3_2QwnNUbPJFDY&usqp=CAU",
-        }}
-        displayButtonAdvance={true}
-        title='QuanHH'
-      />
-      <ProfileItem
-        avatar={{
-          source:"https://phunugioi.com/wp-content/uploads/2020/10/hinh-anh-chibi-buon.jpg",
-        }}
-        displayButtonAdvance={true}
-        title='NXTung'
-      />
-
-      <ProfileItem
-        avatar={{
-          source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7vFTlbgSjNtZgYf3JdY6F7qW4wE_FR2ONaw&usqp=CAU",
-        }}
-        displayButtonAdvance={true}
-        title='TrungVH'
-      />
-
-      <ProfileItem
-        avatar={{
-          source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQf5pUFWedzehaMYG74PAWQ3shRdtygnHDfNg&usqp=CAU",
-        }}
-        displayButtonAdvance={true}
-        title='A Girl'
-      />
-    </View>
+    <>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.container}>
+          {listFriend.map( friend => (
+            <ProfileItem
+              // avatar={friend.avatar.filename}
+              key={friend._id} 
+              avatar={{
+                source:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWx8bMzJMOdeGAEPuEkV3SVhHS6LwUaxVpCy7f3D95lDl8WVRlOewHb3_2QwnNUbPJFDY&usqp=CAU",
+              }}
+              title={friend.username}
+              displayButtonAdvance={true}
+            />
+          ))}
+        </View>
+      </ScrollView>
+    </>
   )
 }
 
