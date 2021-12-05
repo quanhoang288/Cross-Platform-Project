b83b3b87acd1b30e591585a2441cb8c9045cdd42
quanhoang288 <quanpro288@gmail.com>
2021-12-05 14:52:56 +0700

    remove postlist screen and update profile screen

--- src/components/post/PostList.jsx
+++ src/components/post/PostList.jsx
@@ -1,41 +1,30 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
-import { FlatList, View, ScrollView, StyleSheet, RefreshControl } from 'react-native';
-import PostItem from './PostItem';
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { FlatList, StyleSheet, RefreshControl } from "react-native";
+import PostItem from "./PostItem";
 
-const PostList = ({ posts, actionLike, refreshing, handleRefresh }) => {    
-    return (
-        <FlatList
-            data={posts}
-            renderItem={({item}) => {
-                return (
-                    <PostItem 
-                        key={item._id}
-                        post={item}
-                        actionLike={actionLike}
-                    />
-                );
-            }}  
-            keyExtractor={item => item._id}
-            showsVerticalScrollIndicator={false}
-            refreshControl={
-                <RefreshControl
-                    refreshing={refreshing}
-                    onRefresh={handleRefresh}
-                />
-            }
-        />
-    );
+const PostList = ({ posts, refreshing, handleRefresh }) => {
+  return (
+    <FlatList
+      data={posts}
+      renderItem={({ item }) => {
+        return <PostItem key={item._id} post={item} />;
+      }}
+      keyExtractor={(item) => item._id}
+      showsVerticalScrollIndicator={false}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />
+      }
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
-PostList.propTypes = {
-    
-};
+PostList.propTypes = {};
 
 export default PostList;
