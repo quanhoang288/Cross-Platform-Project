4f1f2cca5c3ac64628cbc152c8fd3d36cb5a2094
Quan Hoang <quanpro288@gmail.com>
2021-12-11 14:22:08 +0700

    fix profile rendering bug and add friend status to user info

--- client/src/screens/account/Profile.jsx
+++ client/src/screens/account/Profile.jsx
@@ -16,59 +16,143 @@ import {
   ListItem,
   Text,
 } from "react-native-elements";
-import { useNavigation } from '@react-navigation/core';
+import { useNavigation } from "@react-navigation/core";
 import { PostList } from "../../components/post";
 import { DEVICE_WIDTH } from "../../constants/dimensions";
 import { auth, post } from "../../apis";
 import { useSelector } from "react-redux";
 import { useRoute } from "@react-navigation/core";
 import { stacks } from "../../constants/title";
+import { style } from "styled-system";
 
 const Profile = (props) => {
-  const user = useSelector(state => state.auth.user);
+  const user = useSelector((state) => state.auth.user);
   const route = useRoute();
-  const [posts, setPosts] = useState([]);
-  const [userInfo, setUserInfo] = useState({});
+  const [userData, setUserData] = useState({ info: {}, posts: [] });
+  const [userId, setUserId] = useState(null);
   const navigation = useNavigation();
 
-  // console.log("first");
-
-  const [isOwnProfile, setOwnProfile] = useState(true);
-
-  const fetchUserData = async (userId) => {
+  const fetchUserInfo = async (userId) => {
     try {
       const infoResult = await auth.showInfo(userId, user.token);
-      const postListResult = await post.getListPost(userId, user.token);
-
-      setUserInfo(infoResult.data.data); 
-      setPosts(postListResult.data.data);
-      
+      return infoResult.data.data;
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  const fetchUserPosts = async (userId) => {
+    try {
+      const postListResult = await post.getListPost(userId, user.token);
+      return postListResult.data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    if (route.params && route.params.userId ){
-      setOwnProfile(false);
+    if (route.params && route.params.userId) {
+      setUserId(route.params.userId);
+    } else {
+      setUserId(user.id);
     }
   }, [route]);
 
-  useEffect(()=> {
-    const usedId = isOwnProfile ? user.id : route.params.userId;
-    fetchUserData(usedId);
-  }, [isOwnProfile])
+  useEffect(() => {
+    const initializeUserProfile = async (userId) => {
+      const info = await fetchUserInfo(userId);
+      console.log("user info: ", info);
+      const posts = await fetchUserPosts(userId);
+      setUserData({ info, posts });
+    };
+    if (userId) {
+      initializeUserProfile(userId);
+    }
+  }, [userId]);
 
   const getDate = (value) => {
     let result = "";
     let date = new Date(value);
-    result += `${ date.getDate()}/${date.getMonth()+1}/${date.getFullYear()} `
+    result += `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} `;
     return result;
-  }
+  };
+
+  const renderUserActionButtons = () => {
+    if (!userId) {
+      return null;
+    }
+
+    if (userId === user.id) {
+      return (
+        <>
+          <Button
+            buttonStyle={styles.addPostBtn}
+            title='Add post'
+            icon={{
+              type: "antdesign",
+              name: "pluscircle",
+              color: "rgb(255,255,255)",
+              size: 18,
+            }}
+            titleStyle={{
+              color: "rgb(255,255,255)",
+            }}
+          />
+
+          <Button
+            buttonStyle={styles.updateProfileBtn}
+            title='Update Avatar'
+            icon={{
+              type: "material-community",
+              name: "update",
+              color: "rgb(0,0,0)",
+              size: 18,
+            }}
+            titleStyle={{
+              color: "rgb(0,0,0)",
+            }}
+          />
+        </>
+      );
+    }
+    return (
+      <>
+        <Button
+          buttonStyle={styles.addFriendBtn}
+          title='Add friend'
+          icon={{
+            type: "ionicons",
+            name: "person-add",
+            color: "rgb(255,255,255)",
+            size: 18,
+          }}
+          titleStyle={{
+            fontWeight: "100",
+            color: "rgb(255,255,255)",
+          }}
+        />
+
+        <Button
+          buttonStyle={styles.messageBtn}
+          title='Message'
+          icon={{
+            type: "fontisto",
+            name: "messenger",
+            color: "rgb(255,255,255)",
+            size: 18,
+          }}
+          titleStyle={{
+            fontWeight: "100",
+            color: "rgb(255,255,255)",
+          }}
+        />
+      </>
+    );
+  };
 
   const ProfileHeader = () => {
     return (
-      <>
+      <View style={styles.container}>
         <ImageBackground
           source={{
             uri: "https://mondaycareer.com/wp-content/uploads/2020/11/background-%C4%91%E1%BA%B9p-2-1024x585.jpg",
@@ -88,109 +172,78 @@ const Profile = (props) => {
             />
           </View>
         </View>
-        <Text style={styles.name}> {userInfo.username} </Text>
+        <Text style={styles.name}> {userData.info.username} </Text>
 
-        <View style={styles.buttonGroupContainer}>
-          <View style={styles.buttonGroup}>
-            <Button
-              buttonStyle={{
-                backgroundColor: isOwnProfile ? "rgb(51, 133, 255)" : "rgb(230, 230, 230)",
-                borderRadius: 8,
-                width: "75%",
-                minWidth: 150,
-                padding: 6,
-                marginHorizontal: 6
-              }}
-              
-              title= {isOwnProfile ? "Add story" : 'Add friend'}
-              icon={{
-                type: isOwnProfile ? "antdesign" : "ionicons",
-                name: isOwnProfile ? "pluscircle" : "person-add",
-                color: isOwnProfile ? "rgb(255,255,255)" : "rgb(0,0,0)",
-                size: 18,
-              }}
-              titleStyle={{
-                fontWeight: '100',
-                color: isOwnProfile ? "rgb(255,255,255)" : "rgb(0,0,0)",
-              }}
-            />
-
-            <Button
-              buttonStyle={{
-                backgroundColor: isOwnProfile ? "rgb(230, 230, 230)" : "rgb(51, 133, 255)",
-                borderRadius: 8,
-                minWidth: 150,
-                width: "75%",
-                padding: 6,
-                marginHorizontal: 6
-              }}
-              title={ isOwnProfile ? "Update Avatar " : 'Message'}
-              icon={{
-                type: isOwnProfile ? "material-community" : "fontisto",
-                name: isOwnProfile ? "update" : "messenger",
-                color: isOwnProfile ? "rgb(0,0,0)" : "rgb(255,255,255)",
-                size: 18,
-              }}
-              titleStyle={{
-                fontWeight: '100',
-                color: isOwnProfile ? "rgb(0,0,0)" : "rgb(255,255,255)",
-              }}
-            />
-            <Icon
-              type='feather'
-              name='more-horizontal'
-              size={16}
-              // style={{ marginHorizontal: 6, marginTop: 6, }}
-              containerStyle={{
-                backgroundColor: "rgb(230, 230, 230)", 
-                borderRadius: 8, 
-                justifyContent: "center", 
-                width: 40,
-                marginLeft: 6,
-                // padding: 6,
-              }}
-              onPress={() => {}}
-            />
-          </View>
+        <View style={styles.buttonGroup}>
+          {renderUserActionButtons()}
+          <Icon
+            type='feather'
+            name='more-horizontal'
+            size={16}
+            containerStyle={styles.advanceIconContainer}
+            onPress={() => {}}
+          />
         </View>
 
         <View>
-          <ListItem containerStyle={{paddingVertical: 6}}>
-            <Icon type='antdesign' name='contacts' color="rgb(100,100,100)" size={24}/>
+          <ListItem containerStyle={{ paddingVertical: 6 }}>
+            <Icon
+              type='antdesign'
+              name='contacts'
+              color='rgb(100,100,100)'
+              size={24}
+            />
             <ListItem.Content>
-              <ListItem.Title>{`Contact: ${userInfo.phonenumber}`}</ListItem.Title>
+              <ListItem.Title>{`Contact: ${userData.info.phonenumber}`}</ListItem.Title>
             </ListItem.Content>
           </ListItem>
 
-          <ListItem containerStyle={{paddingVertical: 6}}>
-            <Icon type='font-awesome' name='birthday-cake' color="rgb(100,100,100)" size={24}/>
+          <ListItem containerStyle={{ paddingVertical: 6 }}>
+            <Icon
+              type='font-awesome'
+              name='birthday-cake'
+              color='rgb(100,100,100)'
+              size={24}
+            />
             <ListItem.Content>
-              <ListItem.Title>{`Birthday: ${userInfo.birthday ? getDate(userInfo.birthday) : ""}`}</ListItem.Title>
+              <ListItem.Title>{`Birthday: ${
+                userData.info.birthday ? getDate(userData.info.birthday) : ""
+              }`}</ListItem.Title>
             </ListItem.Content>
           </ListItem>
 
-          <TouchableOpacity 
-            onPress={() => navigation.navigate( isOwnProfile ? stacks.friendTabs.name : stacks.listFriend.name, {
-              userId: isOwnProfile ? userInfo.id : route.params.userId,
-            })}
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate(
+                userId === user.id
+                  ? stacks.friendTabs.name
+                  : stacks.listFriend.name,
+                {
+                  userId,
+                }
+              )
+            }
           >
-            <ListItem containerStyle={{paddingVertical: 6, marginBottom: 20}}>
-              <Icon type='font-awesome-5' name='user-friends' color="rgb(100,100,100)" size={20}/>
+            <ListItem containerStyle={{ paddingVertical: 6, marginBottom: 20 }}>
+              <Icon
+                type='font-awesome-5'
+                name='user-friends'
+                color='rgb(100,100,100)'
+                size={20}
+              />
               <ListItem.Content>
-                <ListItem.Title >Friends</ListItem.Title>
+                <ListItem.Title>Friends</ListItem.Title>
                 <ListItem.Subtitle>{`20 friends`}</ListItem.Subtitle>
               </ListItem.Content>
               <ListItem.Chevron color='black' />
             </ListItem>
           </TouchableOpacity>
         </View>
-      </>
-    )
-  }
+      </View>
+    );
+  };
 
-  return (
-      <PostList posts={posts} header={ProfileHeader}/>
-  );
+  return <PostList posts={userData.posts} header={ProfileHeader} />;
 };
 
 const styles = StyleSheet.create({
@@ -203,36 +256,66 @@ const styles = StyleSheet.create({
     alignItems: "flex-end",
   },
   profileOutterContainer: {
-    backgroundColor:"rgb(255,255,255)",
+    backgroundColor: "rgb(255,255,255)",
     alignItems: "center",
     paddingBottom: 6,
     position: "relative",
   },
   profileInnerContainer: {
-    // backgroundColor:"rgb(255,255,255)",
     marginTop: -50,
-    // width: "fit-content"
   },
 
   name: {
-    backgroundColor:"rgb(255,255,255)",
+    backgroundColor: "rgb(255,255,255)",
     textAlign: "center",
     fontSize: 32,
     fontWeight: "bold",
   },
-  buttonGroupContainer: {
-    alignItems: "center",
-    backgroundColor:"rgb(255,255,255)",
-    paddingBottom: 10
-  },
-  buttonGroup: {
-    width: "80%",
-    flexDirection: "row",
-    marginVertical: 10,
-    justifyContent: "center",
-    // alignItems: 'center',
-  },
 
+  buttonGroup: {
+    backgroundColor: "rgb(255,255,255)",
+    width: "100%",
+    flexDirection: "row",
+    marginVertical: 20,
+    justifyContent: "center",
+  },
+  addPostBtn: {
+    backgroundColor: "rgb(51, 133, 255)",
+    borderRadius: 8,
+    width: "50%",
+    minWidth: 120,
+    padding: 6,
+    marginRight: 6,
+  },
+  updateProfileBtn: {
+    backgroundColor: "rgb(230, 230, 230)",
+    borderRadius: 8,
+    minWidth: 150,
+    width: "50%",
+    padding: 6,
+  },
+  addFriendBtn: {
+    backgroundColor: "rgb(51, 133, 255)",
+    borderRadius: 8,
+    width: "50%",
+    minWidth: 150,
+    padding: 6,
+    marginRight: 6,
+  },
+  messageBtn: {
+    backgroundColor: "rgb(51, 133, 255)",
+    borderRadius: 8,
+    minWidth: 150,
+    width: "50%",
+    padding: 6,
+  },
+  advanceIconContainer: {
+    backgroundColor: "rgb(230, 230, 230)",
+    borderRadius: 8,
+    justifyContent: "center",
+    width: 40,
+    marginLeft: 6,
+  },
 });
 
 Profile.propTypes = {};
