a83cd688f2bad49fa502fc6d7bc46046dd355ee3
quanhoang288 <quanpro288@gmail.com>
2022-01-08 15:48:00 +0700

    fix comment and profile pagination

--- server/controllers/PostComment.js
+++ server/controllers/PostComment.js
@@ -2,53 +2,52 @@ const PostModel = require("../models/Posts");
 const PostCommentModel = require("../models/PostComment");
 const httpStatus = require("../utils/httpStatus");
 const getPaginationParams = require("../utils/getPaginationParams");
+const { STATUS_DELETED } = require("../constants/constants");
 const postCommentController = {};
 postCommentController.create = async (req, res, next) => {
+  const { content, commentAnswered } = req.body;
+  let userId = req.userId;
+  let post;
   try {
-    let userId = req.userId;
-    let post;
-    try {
-      post = await PostModel.findById(req.params.postId);
-      if (post == null) {
-        return res
-          .status(httpStatus.NOT_FOUND)
-          .json({ message: "Can not find post" });
-      }
-
-      if ((post.status = STATUS_DELETED)) {
-        return res
-          .status(httpStatus.GONE)
-          .json({ message: "This post has been deleted" });
-      }
-    } catch (error) {
+    post = await PostModel.findById(req.params.postId);
+    if (post == null) {
       return res
-        .status(httpStatus.INTERNAL_SERVER_ERROR)
-        .json({ message: error.message });
+        .status(httpStatus.NOT_FOUND)
+        .json({ message: "Can not find post" });
     }
-    const { content, commentAnswered } = req.body;
+
+    if (post.status == STATUS_DELETED) {
+      return res
+        .status(httpStatus.GONE)
+        .json({ message: "This post has been deleted" });
+    }
 
     const postComment = new PostCommentModel({
       user: userId,
       post: post._id,
       content: content,
-      commentAnswered: commentAnswered ? commentAnswered : null,
+      commentAnswered: commentAnswered || null,
     });
 
     let postCommentSaved = await postComment.save();
-    // update countComments post
-    console.log(req.params.postId);
-    console.log(post.countComments ? post.countComments + 1 : 1);
-    let postSaved = await PostModel.findByIdAndUpdate(req.params.postId, {
+
+    // update countComments of post
+    await PostModel.findByIdAndUpdate(req.params.postId, {
       countComments: post.countComments ? post.countComments + 1 : 1,
     });
     postCommentSaved = await PostCommentModel.findById(
       postCommentSaved._id
-    ).populate("user", ["username", "phonenumber"]);
+    ).populate({
+      path: "user",
+      select: "_id username phonenumber",
+      model: "Users",
+    });
+
     return res.status(httpStatus.OK).json({
       data: postCommentSaved,
-      post: postSaved,
     });
   } catch (e) {
+    console.log(e.message);
     return res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
       message: e.message,
     });
